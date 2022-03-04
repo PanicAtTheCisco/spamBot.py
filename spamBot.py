52e94000bec9fb6aac1750a825a168fb9befca17
PanicAtTheCisco <83227932+PanicAtTheCisco@users.noreply.github.com>
2022-03-03 21:19:47 -0600

    Update spamBot.py

--- spamBot.py
+++ spamBot.py
@@ -2,7 +2,7 @@ import pyautogui, time
 
 def main():
     script = input("Enter text file path: ")     #<-- use '.txt' files as the program reads them easier
-    #note: you will need to copy the file path of the file to use it 
+    #note: you will need to copy the file path of the file to use it unless it is in the same folder as this program
     print("Running...")
 
     time.sleep(5)     #<-- gives the user time to switch to whatever application they want to use the program on and can be changed as needed
@@ -11,7 +11,7 @@ def main():
     for word in f:
         pyautogui.typewrite(word)
         pyautogui.press("enter")
-        time.sleep(1.25)     #<-- used to bypass discord slowmode (for the most part) though can be changed as needed
+        time.sleep(1.25)     #<-- can be changed as needed
 
 print("Online.")
 time.sleep(1)
